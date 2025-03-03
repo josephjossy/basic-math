125818eef225ba4fb25b674d47f2f6ee3b2ac1e1
josephjossy <joseph.jossy01@gmail.com>
2025-03-03 16:32:45 -0500

    third as a package

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # second impelmentation as a module
-from functions import add, subtract, multiply, divide
+from functions.functions import add, subtract, multiply, divide
+from functions.numbers import num1,num2
 
-sum = add(2,3)
-difference = subtract(2,3)
-product = multiply(2,3)
-division = divide(2,3)
+
+sum = add(num1,num2)
+difference = subtract(num1,num2)
+product = multiply(num1,num2)
+division = divide(num1,num2)
 
 print(sum,difference,product,division)
