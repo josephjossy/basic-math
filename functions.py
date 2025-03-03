ee4e4da15486dd011fe9d600dcc528ed701fe378
josephjossy <joseph.jossy01@gmail.com>
2025-03-03 16:12:12 -0500

    second way as a module

---
+++ functions.py
@@ -0,0 +1,14 @@
+def add(num1, num2):
+    return int(num1+num2)
+
+
+def subtract(num1, num2):
+    return int(num1-num2)
+
+
+def multiply(num1, num2):
+    return int(num1*num2)
+
+
+def divide(num1, num2):
+    return float(int(num1)/int(num2))
--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-# first impelmentation as a file
-
-def add(num1, num2):
-    return int(num1+num2)
-
-
-def subtract(num1, num2):
-    return int(num1-num2)
-
-
-def multiply(num1, num2):
-    return int(num1*num2)
-
-
-def divide(num1, num2):
-    return float(int(num1)/int(num2))
-
+# second impelmentation as a module
+from functions import add, subtract, multiply, divide
 
 sum = add(2,3)
 difference = subtract(2,3)
